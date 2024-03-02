76e91a5f3904a975ecf576e381faca8ad41a2d76
joao-oliveira55 <ash.joao55@gmail.com>
2024-03-02 11:52:47 -0300

    feat: last Post manifest

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -35,29 +35,30 @@ import * as $$$$$$25 from "./sections/Images/BannerGrid.tsx";
 import * as $$$$$$26 from "./sections/Images/Carousel.tsx";
 import * as $$$$$$27 from "./sections/Images/ImageGallery.tsx";
 import * as $$$$$$28 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$29 from "./sections/Layout/Container.tsx";
-import * as $$$$$$30 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$31 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$32 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$33 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$34 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$35 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$36 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$37 from "./sections/Miscellaneous/Slide.tsx";
-import * as $$$$$$38 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$39 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$40 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$41 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$42 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$43 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$44 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$45 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$46 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$47 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$48 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$49 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$50 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$51 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$29 from "./sections/LatestPosts/LatestPosts.tsx";
+import * as $$$$$$30 from "./sections/Layout/Container.tsx";
+import * as $$$$$$31 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$32 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$33 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$34 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$35 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$36 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$37 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$38 from "./sections/Miscellaneous/Slide.tsx";
+import * as $$$$$$39 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$40 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$41 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$42 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$43 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$44 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$45 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$46 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$47 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$48 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$49 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$50 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$51 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$52 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -96,30 +97,33 @@ const manifest = {
     "deco-sites/cenoura-site/sections/Images/Carousel.tsx": $$$$$$26,
     "deco-sites/cenoura-site/sections/Images/ImageGallery.tsx": $$$$$$27,
     "deco-sites/cenoura-site/sections/Images/ShoppableBanner.tsx": $$$$$$28,
-    "deco-sites/cenoura-site/sections/Layout/Container.tsx": $$$$$$29,
-    "deco-sites/cenoura-site/sections/Layout/Flex.tsx": $$$$$$30,
-    "deco-sites/cenoura-site/sections/Layout/Grid.tsx": $$$$$$31,
-    "deco-sites/cenoura-site/sections/Layout/GridItem.tsx": $$$$$$32,
-    "deco-sites/cenoura-site/sections/Links/LinkTree.tsx": $$$$$$33,
-    "deco-sites/cenoura-site/sections/Links/Shortcuts.tsx": $$$$$$34,
-    "deco-sites/cenoura-site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$35,
-    "deco-sites/cenoura-site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$36,
-    "deco-sites/cenoura-site/sections/Miscellaneous/Slide.tsx": $$$$$$37,
-    "deco-sites/cenoura-site/sections/Newsletter/Newsletter.tsx": $$$$$$38,
+    "deco-sites/cenoura-site/sections/LatestPosts/LatestPosts.tsx": $$$$$$29,
+    "deco-sites/cenoura-site/sections/Layout/Container.tsx": $$$$$$30,
+    "deco-sites/cenoura-site/sections/Layout/Flex.tsx": $$$$$$31,
+    "deco-sites/cenoura-site/sections/Layout/Grid.tsx": $$$$$$32,
+    "deco-sites/cenoura-site/sections/Layout/GridItem.tsx": $$$$$$33,
+    "deco-sites/cenoura-site/sections/Links/LinkTree.tsx": $$$$$$34,
+    "deco-sites/cenoura-site/sections/Links/Shortcuts.tsx": $$$$$$35,
+    "deco-sites/cenoura-site/sections/Miscellaneous/CampaignTimer.tsx":
+      $$$$$$36,
+    "deco-sites/cenoura-site/sections/Miscellaneous/CookieConsent.tsx":
+      $$$$$$37,
+    "deco-sites/cenoura-site/sections/Miscellaneous/Slide.tsx": $$$$$$38,
+    "deco-sites/cenoura-site/sections/Newsletter/Newsletter.tsx": $$$$$$39,
     "deco-sites/cenoura-site/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$39,
-    "deco-sites/cenoura-site/sections/Product/ImageGallerySlider.tsx": $$$$$$40,
-    "deco-sites/cenoura-site/sections/Product/NotFound.tsx": $$$$$$41,
-    "deco-sites/cenoura-site/sections/Product/NotFoundChallenge.tsx": $$$$$$42,
-    "deco-sites/cenoura-site/sections/Product/ProductInfo.tsx": $$$$$$43,
-    "deco-sites/cenoura-site/sections/Product/ProductShelf.tsx": $$$$$$44,
-    "deco-sites/cenoura-site/sections/Product/ProductShelfTabbed.tsx": $$$$$$45,
-    "deco-sites/cenoura-site/sections/Product/SearchResult.tsx": $$$$$$46,
-    "deco-sites/cenoura-site/sections/Product/ShelfWithImage.tsx": $$$$$$47,
-    "deco-sites/cenoura-site/sections/Product/Wishlist.tsx": $$$$$$48,
-    "deco-sites/cenoura-site/sections/Social/InstagramPosts.tsx": $$$$$$49,
-    "deco-sites/cenoura-site/sections/Social/WhatsApp.tsx": $$$$$$50,
-    "deco-sites/cenoura-site/sections/Theme/Theme.tsx": $$$$$$51,
+      $$$$$$40,
+    "deco-sites/cenoura-site/sections/Product/ImageGallerySlider.tsx": $$$$$$41,
+    "deco-sites/cenoura-site/sections/Product/NotFound.tsx": $$$$$$42,
+    "deco-sites/cenoura-site/sections/Product/NotFoundChallenge.tsx": $$$$$$43,
+    "deco-sites/cenoura-site/sections/Product/ProductInfo.tsx": $$$$$$44,
+    "deco-sites/cenoura-site/sections/Product/ProductShelf.tsx": $$$$$$45,
+    "deco-sites/cenoura-site/sections/Product/ProductShelfTabbed.tsx": $$$$$$46,
+    "deco-sites/cenoura-site/sections/Product/SearchResult.tsx": $$$$$$47,
+    "deco-sites/cenoura-site/sections/Product/ShelfWithImage.tsx": $$$$$$48,
+    "deco-sites/cenoura-site/sections/Product/Wishlist.tsx": $$$$$$49,
+    "deco-sites/cenoura-site/sections/Social/InstagramPosts.tsx": $$$$$$50,
+    "deco-sites/cenoura-site/sections/Social/WhatsApp.tsx": $$$$$$51,
+    "deco-sites/cenoura-site/sections/Theme/Theme.tsx": $$$$$$52,
   },
   "apps": {
     "deco-sites/cenoura-site/apps/decohub.ts": $$$$$$$$$$$0,
